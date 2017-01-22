4fe6d5272f4b28afc212ffb11246ace63869edcc
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-22 13:34:35 +0100

    Configuramos notificaciones y Browser Sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,32 @@
-var gulp = require('gulp');  //Importamos gulp
+var gulp = require('gulp');  //Import gulp
 var sass = require('gulp-sass'); //Import sass
+var notify = require('gulp-notify');
+var browserSync = require('browser-sync').create();
 
-//definimos la tarea por defecto
-gulp.task("default", function() {
+//Definimos la tarea por defecto
+gulp.task("default", ["compile-sass"], function() {
+
+    // Arrancar eñ servidor Browser sync
+    browserSync.init({
+        server:"./"
+    });
+
     //Cuando haya cambios en style.scss, compila sass
     gulp.watch('./src/scss/style.scss', ['compile-sass']);
+
+    //Cuando se cambie el HTML, Recargamos el navegador
+    gulp.watch('./*.html', function() {
+        browserSync.reload();
+        notify().write("Navegador recargado");      //Mostramos notificación
+    });     
 });
 
 gulp.task("compile-sass", function(){
     gulp.src('./src/scss/style.scss')  //Cargo el style.scss
-    .pipe(sass().on('error', sass.logError)) //compilo sass
-    .pipe(gulp.dest('./dist/'));   //Dejo el resultado en ./
+    .pipe(sass().on('error', function(error) {  //Compilamos sass
+        return notify().write(error);  //Si ocurre un error, mostramos notificación
+    })) 
+    .pipe(gulp.dest('./dist/'))   //Dejo el resultado en ./dist
+    .pipe(browserSync.stream())   // Recargamos el CSS en el navegador
+    .pipe(notify("SASS Compilado"));
 });
