05c04640053b73367aa955291319f0a11cf67af4
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-22 21:41:02 +0100

    Añadimos la tarea de concatenar js con Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,31 +2,70 @@ var gulp = require('gulp');  //Import gulp
 var sass = require('gulp-sass'); //Import sass
 var notify = require('gulp-notify');
 var browserSync = require('browser-sync').create();
+var concat = require('gulp-concat');
+var browserify = require('browserify');
+var tap = require('gulp-tap');
+var buffer = require('gulp-buffer');
 
-//Definimos la tarea por defecto
-gulp.task("default", ["compile-sass"], function() {
+// config
+var sassConfig = {
+    compileSassTaskName: 'compile-sass',
+    watchFiles: './src/scss/*.scss',
+    entryPoint: './src/scss/style.scss',
+    dest: './dist/'
+};
 
-    // Arrancar eñ servidor Browser sync
+var jsConfig = {
+    concatJsTaskName: 'concat-js',
+    watchFiles: './src/js/*.js',
+    entryPoint: './src/js/*.js',
+    concatFile: 'main.js',
+    dest: './dist/'
+};
+
+
+// definimos la tarea por defecto
+gulp.task("default", [sassConfig.compileSassTaskName, jsConfig.concatJsTaskName], function(){
+
+    // arrancar el servidor de browser sync
     browserSync.init({
-        server:"./"
+        server: "./"
     });
 
-    //Cuando haya cambios en archivos scss, compila sass
-    gulp.watch('./src/scss/*.scss', ['compile-sass']);
+    // cuando haya cambios en archivos scss, compila sass
+    gulp.watch(sassConfig.watchFiles, [sassConfig.compileSassTaskName]);
 
-    //Cuando se cambie el HTML, Recargamos el navegador
-    gulp.watch('./*.html', function() {
-        browserSync.reload();
-        notify().write("Navegador recargado");      //Mostramos notificación
-    });     
+    // cuando haya cambios en archivos js, los concatena
+    gulp.watch(jsConfig.watchFiles, [jsConfig.concatJsTaskName]);
+
+    // cuando se cambie el html, recarga el navegador
+    gulp.watch('./*.html', function(){
+        browserSync.reload();  // recarga navegador
+        notify().write("Navegador recargado"); // mostramos notificación
+    });
 });
 
-gulp.task("compile-sass", function(){
-    gulp.src('./src/scss/style.scss')  //Cargo el style.scss
-    .pipe(sass().on('error', function(error) {  //Compilamos sass
-        return notify().write(error);  //Si ocurre un error, mostramos notificación
-    })) 
-    .pipe(gulp.dest('./dist/'))   //Dejo el resultado en ./dist
-    .pipe(browserSync.stream())   // Recargamos el CSS en el navegador
-    .pipe(notify("SASS Compilado"));
+// compila sass
+gulp.task(sassConfig.compileSassTaskName, function(){
+    gulp.src(sassConfig.entryPoint)    // cargo el style.scss
+    .pipe(sass().on('error', function(error){ // compilamos sass
+        return notify().write(error); // si ocurre un error, mostramos notifiación
+    }))
+    .pipe(gulp.dest(sassConfig.dest))      // dejo el resultado en ./dist/
+    .pipe(browserSync.stream())     // recargamos el CSS en el navegador
+    .pipe(notify("SASS Compilado 🤘"));
+});
+
+// concatena js
+gulp.task(jsConfig.concatJsTaskName, function(){
+    gulp.src(jsConfig.entryPoint)
+    .pipe(tap(function(file){ // para cada archivo seleccionado
+        // lo pasamos por browserify para importar los require
+        file.contents = browserify(file.path).bundle();
+    }))
+    .pipe(buffer()) // convertimos a buffer para que funcione el siguiente pipe
+    .pipe(concat(jsConfig.concatFile))
+    .pipe(gulp.dest(jsConfig.dest))
+    .pipe(notify("JS Concatenado 💪"))
+    .pipe(browserSync.stream());
 });
