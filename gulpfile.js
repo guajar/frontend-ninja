c916df69697f8952dc3041ae8b9a80ac98e27b8b
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-01-23 22:28:58 +0100

    Añadimos sparRest y modificamos BrowserSync para hacer de proxy

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,8 @@ gulp.task("default", [sassConfig.compileSassTaskName, jsConfig.concatJsTaskName]
 
     // arrancar el servidor de browser sync
     browserSync.init({
-        server: "./"
+        //server: "./"
+        proxy: "127.0.0.1:8000"     //Conectar browserSync con sparRest(Backend)
     });
 
     // cuando haya cambios en archivos scss, compila sass
